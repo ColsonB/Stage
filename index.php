b57b28af90171442d8da6d4a0bef3f46451b6608
ColsonB <bcolson@la-providence.net>
2021-06-03 09:13:18 +0200

    Modification

--- index.php
+++ index.php
@@ -1,48 +1,56 @@
 <?php
-    try{
+    try{ //Connexion à la base de données
         $BDD=new PDO('mysql:host=localhost; dbname=stage; charset=utf8','root','');
     }catch(Exception $e){
         die('Erreur : ' . $e->getMessage());
     }    
-           
-    $req_analyse = $BDD->prepare("SELECT DISTINCT analyse FROM analyse ORDER BY analyse");
+    
+    $i=0;
+
+    //Requête pour récupérer l'analyse
+    $req_analyse = $BDD->prepare('SELECT DISTINCT analyse FROM analyse');
     $req_analyse->execute();
-    $count_analyse = $req_analyse->rowCount();
+    $count_analyse = $req_analyse->rowCount(); //Retourne le nombre de ligne
 
-        $i=0;
-        
-    while($tab_analyse=$req_analyse->fetch(PDO::FETCH_ASSOC)){
+    while($analyse=$req_analyse->fetch(PDO::FETCH_ASSOC)){ //Permet de remplir le tableau analyse
+    //FETCH_ASSOC retourne un tableau indexé par le nom de la colonne
 
-        $req_parametre = $BDD->prepare("SELECT DISTINCT analyse FROM analyse WHERE analyse = '%s'");
-        $req_parametre->bindParam('%s', $tab_analyse['analyse']);
+        //Requête pour récupérer le paramétre de l'analyse
+        $req_parametre = $BDD->prepare('SELECT parametre FROM analyse WHERE analyse=:nom');
+        $req_parametre->bindParam(':nom', $analyse['analyse']);
         $req_parametre->execute();
+
         $count_parameter = $req_parametre->rowCount();
 
-        $tab_titre[$i]=$tab_analyse['analyse'];
+
+        $tab_titre[$i]=$analyse['analyse'];//On mets l'analyse dans le tableau
         $tab[0][$i]="Analyse";
+        $i++;
 
-        while($parametre = $req_parametre->fetch(PDO::FETCH_NUM)){
+        while($parametre = $req_parametre->fetch(PDO::FETCH_NUM)){ //Permet de remplir le tableau analyse
+        //FETCH_NUM retourne un tableau indexé par le numéro de la colonne
 
-            $tab_titre[$i]=$parametre[0];
+            $tab_titre[$i]=$parametre[0];//On mets le paramétre dans le tableau
             $tab[0][$i]="Parametre";
             $i++;
 
-            $tab_titre[$i]="resultat";
+            $tab_titre[$i]='resultat';
             $tab[0][$i]="Resultat";
             $i++;
-
+  
+            }
         }
-    }
 
     $z=1;
     $j=0;
 
-    $query_distinct_method = $BDD->prepare("SELECT DISTINCT analyse FROM analyse WHERE analyse = '%s'");
-    $query_distinct_method->execute();
+    $query_distinct_analyse = $BDD->prepare("SELECT DISTINCT analyse FROM analyse ORDER BY analyse");
+    $query_distinct_analyse->execute();
 
-    while($distinct_method=$query_distinct_method->fetch(PDO::FETCH_NUM)){
-        
-        $query_parameter = $BDD->prepare("SELECT DISTINCT parametre, resultat FROM analyse WHERE analyse = '%s'");
+    while($distinct_method=$query_distinct_analyse->fetch(PDO::FETCH_NUM)){
+
+        $query_parameter = $BDD->prepare('SELECT DISTINCT parametre, resultat FROM analyse WHERE analyse=:tab');
+        $query_parameter->bindParam(':tab', $distinct_method[0]);
         $query_parameter->execute();
 
         for($x=0; $x<$i; $x++){
@@ -63,16 +71,16 @@
 
                         if($tab_titre[$j]==$parametre[0]){
                             
-                            $tab[$i][$j]=$parametre[0];
+                            $tab[$z][$j]=$parametre[0];
                             $j++;
 
-                            $tab[$i][$j]=$parametre[1];
+                            $tab[$z][$j]=$parametre[1];
                             $j++;
 
-                            $tab[$i][$j]=$parametre[2];
+                            $tab[$z][$j]=$parametre[2];
                             $j++;
 
-                            $tab[$i][$j]=$parametre[3];
+                            $tab[$z][$j]=$parametre[3];
                             $j++;
                         }
                         else{
@@ -87,19 +95,13 @@
     }
 
     $j=0;
-    $i++;
+    $z++;
 
-    foreach($tab as $cle=>$ligne){
-        foreach($ligne as $key=>$valeur){
-            if($ind == 0){
-                $lecture = $tab[$cle][$valeur];
-            }
-            else{
-                $lecture = $lecture.','.$valeur;
-            }
-        }
-        $lecture = $lecture."\n";
-        echo $lecture;
+    $z=1;
+
+    foreach($tab_titre as $cle=>$ligne){ //Permet de lire le tableau $tab_titre
+        echo $ligne.',';
     }
-    var_dump($tab); 
+
+    var_dump($tab_titre);
 ?>
