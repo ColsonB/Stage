5c7fe6d85cd270a4b15762ae376e32110e680b55
ColsonB <bcolson@la-providence.net>
2021-06-18 10:33:17 +0200

    Modification

--- tableau.ps1
+++ tableau.ps1
@@ -5,15 +5,16 @@
     $objExcelFileOpen = $objExcel.Workbooks.Open($PathExcelSrc) # Ouverture du fichier Excel
     $objExcelSheetOpen = $objExcelFileOpen.sheets.item($Sheet)
 
-    $objExcelSheetOpen.cells.item(1,3).text
-
     for($i=1; $i -le 4; $i++){ # On parcourt les 4 lignes du tableau
 
         Write-Host $objExcelSheetOpen.cells.Item($i, 1).text # On affiche les valeurs de la premiére colonne
         Write-Host $objExcelSheetOpen.cells.Item($i, 2).text
         Write-Host $objExcelSheetOpen.cells.Item($i, 3).text
+        Write-Host $objExcelSheetOpen.cells.Item($i, 4).text
 
     }
+
+    $objExcelSheetOpen.cells.Item(1, 5) = 'X' # Mets une croix dans la ligne 1 colonne 5
 
     $objExcelFileOpen.close() # On ferme le fichier Excel
     $objExcel.Quit()
