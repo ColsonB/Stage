d90a7948734efc9662d5af9b652bec868a717d78
ColsonB <bcolson@la-providence.net>
2021-06-18 09:47:45 +0200

    Ajout de commentaire

--- tableau.ps1
+++ tableau.ps1
@@ -7,7 +7,7 @@
 
     $objExcelSheetOpen.cells.item(1,3).text
 
-    for($i=1; $i -le 4; $i++){ # On parcoure les 4 lignes du tableau
+    for($i=1; $i -le 4; $i++){ # On parcourt les 4 lignes du tableau
 
         Write-Host $objExcelSheetOpen.cells.Item($i, 1).text # On affiche les valeurs de la premiére colonne
         Write-Host $objExcelSheetOpen.cells.Item($i, 2).text
