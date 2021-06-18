2131bdf5f1f3933afcda9a932c2b4fe5453fcc10
ColsonB <bcolson@la-providence.net>
2021-06-18 09:47:26 +0200

    Ajout de commentaire

--- tableau.ps1
+++ tableau.ps1
@@ -1,34 +1,19 @@
-<# 
-    $mdarray1 = @()
-    $mdarray1_counter ++
-    $mdarray1 += ,@($mdarray1_counter, 'Ana 1', 'Ph')
-    $mdarray1_counter ++
-    $mdarray1 += ,@($mdarray1_counter, 'Ana 2', 'Mg')
-    $mdarray1_counterr ++
-    $mdarray1 += ,@($mdarray1_counter, 'Ana 2', 'Zn')
-    $mdarray1_counter ++
-    $mdarray1 += ,@($mdarray1_counter, 'Ana 3', 'Mn')
-    
-    foreach($array10 in $mdarray1){
-        Write-host ($array10)
-    } 
-#>
-
     $PathExcelSrc = "C:\Users\Obsti\Documents\test.xlsx"
     $Sheet = "Feuil1"
 
     $objExcel = New-Object -ComObject Excel.Application
-    $objExcelFileOpen = $objExcel.Workbooks.Open($PathExcelSrc)
+    $objExcelFileOpen = $objExcel.Workbooks.Open($PathExcelSrc) # Ouverture du fichier Excel
     $objExcelSheetOpen = $objExcelFileOpen.sheets.item($Sheet)
 
     $objExcelSheetOpen.cells.item(1,3).text
 
-    for($i=1; $i -le 4; $i++){
+    for($i=1; $i -le 4; $i++){ # On parcoure les 4 lignes du tableau
 
-        Write-Host $objExcelSheetOpen.cells.Item($i, 1).text
+        Write-Host $objExcelSheetOpen.cells.Item($i, 1).text # On affiche les valeurs de la premiére colonne
         Write-Host $objExcelSheetOpen.cells.Item($i, 2).text
+        Write-Host $objExcelSheetOpen.cells.Item($i, 3).text
 
     }
 
-    $objExcelFileOpen.close()
+    $objExcelFileOpen.close() # On ferme le fichier Excel
     $objExcel.Quit()
