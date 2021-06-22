53eb18873eec02d136dd517e62da50827e04ea88
ColsonB <bcolson@la-providence.net>
2021-06-22 10:01:56 +0200

    Modification

--- tableau.ps1
+++ tableau.ps1
@@ -12,12 +12,53 @@
         Write-Host $objExcelSheetOpen.cells.Item($i, 3).text
         Write-Host $objExcelSheetOpen.cells.Item($i, 4).text
 
-        if($objExcelSheetOpen.cells.Item($i, 3).text -eq ""){
-
-            $objExcelSheetOpen.cells.Item(3, 3) = "X"
-
+        if($objExcelSheetOpen.cells.Item($i, 1).text -eq ""){
+            if($objExcelSheetOpen.cells.Item(2, 1).text -eq ""){
+                $objExcelSheetOpen.cells.Item(2, 1) = "X"  
+            }
+            if($objExcelSheetOpen.cells.Item(3, 1).text -eq ""){
+                $objExcelSheetOpen.cells.Item(3, 1) = "X"  
+            }
+            if($objExcelSheetOpen.cells.Item(4, 1).text -eq ""){
+                $objExcelSheetOpen.cells.Item(4, 1) = "X"
+            }
         }
 
+        if($objExcelSheetOpen.cells.Item($i, 2).text -eq ""){
+            if($objExcelSheetOpen.cells.Item(2, 2).text -eq ""){
+                $objExcelSheetOpen.cells.Item(2, 2) = "X"  
+            }
+            if($objExcelSheetOpen.cells.Item(3, 2).text -eq ""){
+                $objExcelSheetOpen.cells.Item(3, 2) = "X"  
+            }
+            if($objExcelSheetOpen.cells.Item(4, 2).text -eq ""){
+                $objExcelSheetOpen.cells.Item(4, 2) = "X"
+            }
+        }
+
+        if($objExcelSheetOpen.cells.Item($i, 3).text -eq ""){
+            if($objExcelSheetOpen.cells.Item(2, 3).text -eq ""){
+                $objExcelSheetOpen.cells.Item(2, 3) = "X"  
+            }
+            if($objExcelSheetOpen.cells.Item(3, 3).text -eq ""){
+                $objExcelSheetOpen.cells.Item(3, 3) = "X"  
+            }
+            if($objExcelSheetOpen.cells.Item(4, 3).text -eq ""){
+                $objExcelSheetOpen.cells.Item(4, 3) = "X"
+            }
+        }
+
+        if($objExcelSheetOpen.cells.Item($i, 4).text -eq ""){
+            if($objExcelSheetOpen.cells.Item(2, 4).text -eq ""){
+                $objExcelSheetOpen.cells.Item(2, 4) = "X"  
+            }
+            if($objExcelSheetOpen.cells.Item(3, 4).text -eq ""){
+                $objExcelSheetOpen.cells.Item(3, 4) = "X"  
+            }
+            if($objExcelSheetOpen.cells.Item(4, 4).text -eq ""){
+                $objExcelSheetOpen.cells.Item(4, 4) = "X"
+            }
+        }
     }
 
     # $objExcelSheetOpen.cells.Item(1, 5) = 'X' # Mets une croix dans la ligne 1 colonne 5
