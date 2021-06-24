79f1245e5d81d912ffa1d495d32d660e38bbb480
ColsonB <bcolson@la-providence.net>
2021-06-24 15:04:27 +0200

    Modification

--- tableau.ps1
+++ tableau.ps1
@@ -1,69 +1,192 @@
-    $PathExcelSrc = "C:\Users\Obsti\Documents\test.xlsx"
-    $Sheet = "Feuil1"
-    $Sheet1 = "Feuil2"
+﻿$PathExcelSrc="C:\Users\Obsti\Documents\test.xlsx"
+$Sheet1="Feuil1"
+$Sheet2="Feuil2"
 
-    $objExcel = New-Object -ComObject Excel.Application
-    $objExcelFileOpen = $objExcel.Workbooks.Open($PathExcelSrc) # Ouverture du fichier Excel
-    $objExcelSheetOpen = $objExcelFileOpen.sheets.item($Sheet) 
-    $objExcelSheetOpen = $objExcelFileOpen.sheets.item($Sheet1)
+$objExcel = New-Object -ComObject Excel.Application
+$objExcelFileOpen = $objExcel.Workbooks.Open($PathExcelSrc)
+$objExcelSheetOpen = $objExcelFileOpen.sheets.item($Sheet1)
+$objExcelSheetOpen2 = $objExcelFileOpen.sheets.item($Sheet2)
+$rows = $objExcelSheetOpen.UsedRange.Rows.Count
+$rows2 = $objExcelSheetOpen2.UsedRange.Rows.Count
 
-    for($i=1; $i -le 4; $i++){ # On parcourt les 4 lignes du tableau
+<# 
+On va insérer dans excel en feuil2 une formule pour nous indiquer 
+le nombre de référence distincte en vue de créer le tableau 
+#>
 
-        Write-Host $objExcelSheetOpen.cells.Item($i, 1).text # On affiche les valeurs de la premiére colonne
-        Write-Host $objExcelSheetOpen.cells.Item($i, 2).text
-        Write-Host $objExcelSheetOpen.cells.Item($i, 3).text
-        Write-Host $objExcelSheetOpen.cells.Item($i, 4).text
+$totlignplusun = $rows2 + 1
+$objExcelSheetOpen2.Cells.Item($totlignplusun,1).FormulaLocal = "=SOMMEPROD(1/NB.SI(A1:A$rows2;A1:A$rows2))"
+$objExcelFileOpen.Save() # On sauvegarde la valeur de la formule
+$nbreref = $objExcelSheetOpen2.cells.Item($totlignplusun,1).Value() # On lis la valeur
 
-        if($objExcelSheetOpen.cells.Item($i, 1).text -eq ""){ # Vérifie si une case n'est pas vide dans la premiére colonne
-            if($objExcelSheetOpen.cells.Item(2, 1).text -eq ""){ # Si la case 2 dans la premiére colonne est vide on mets une croix
-                $objExcelSheetOpen.cells.Item(2, 1) = "X"  
-            }
-            if($objExcelSheetOpen.cells.Item(3, 1).text -eq ""){
-                $objExcelSheetOpen.cells.Item(3, 1) = "X"  
-            }
-            if($objExcelSheetOpen.cells.Item(4, 1).text -eq ""){
-                $objExcelSheetOpen.cells.Item(4, 1) = "X"
-            }
+Write-Host "Nombres lignes feuil1"
+write-host $rows
+Write-Host "Nombres lignes feuil2"
+write-host $rows2
+Write-Host "Nombre référence"
+write-host $nbreref
+
+$analyse = @()
+$analyse =$null
+$analyse = @()
+
+<#
+Remplissage tableau analyse
+en lisant la feuil1 du fichier Excel
+Ce tableau va être de la forme
+Ana 1 Ph Ana 2 Mg Ana 3 Zn Ana 4 Cu .......
+#>
+
+for($i=1; $i -le $rows; $i++){
+    $analyse += $objExcelSheetOpen.cells.Item($i, 1).text
+    $analyse += $objExcelSheetOpen.cells.Item($i, 2).text
+    $analyse += "Valeur"
+}
+
+# Affichage tableau analyse
+$nbreanalyse = $analyse.Length
+Write-Host "Nombre élément taleau analyse"
+Write-Host $nbreanalyse
+write-host "Affichage tableau analyse"
+write-host "-------------------------"
+
+for($i=0; $i -lt $nbreanalyse; $i++){
+    write-host $analyse[$i]
+}
+write-host "Fin affichage tableau analyse"
+write-host "-----------------------------"
+
+<#
+Lecture fichier Excel feuil2 pour prendre la colonne 1 qui contient les références variable 
+Le tableau à 2 dimensions tabref (lignes,colonnes)
+lignes=rows2 -> nbre de lignes de la feuil2
+colonnes=nbreanalyse -> nbre éléments du tableau analyse
+On va forcer les colonnes provenant du tableau analyse à '-'
+On force l'indice à 0
+On va avoir un tableau du genre 2070 - - - - - .......
+                                2071 - - - - - .......
+Variable j ligne tableau
+Variable k pour balayer la feuil2 du fichier excel
+Variable w colonne pour incice du tableau                                
+#>
+
+$j=0
+$k=0
+$z=0
+$w=0
+
+$nbretableau=$nbreanalyse+1
+$tabref = $null
+[string[,]]$tabref = [string[,]]::New($nbreref,$nbretableau)
+$var=''
+
+for($k=1; $k -le $rows2; $k++){
+    # On force la position de la référence en indice 0
+    $w=0
+    # On va tester si la référence a déjà été lue dans le fichier feuil2
+    if($var -ne $objExcelSheetOpen2.cells.Item($k, 1).text){
+        $var = $objExcelSheetOpen2.cells.Item($k, 1).text
+        $tabref[$j,$w] = $objExcelSheetOpen2.cells.Item($k, 1).text
+        # On remplit le nombre de colonnes en fonction du nombre d'indices du tableau analyse et on y met des '-'
+        for ($w=0; $w -lt $nbreanalyse; $w++){
+            $z=$w+1
+            $tabref[$j,$z] = '-'
         }
+        $j++
+    }
+}
 
-        if($objExcelSheetOpen.cells.Item($i, 2).text -eq ""){
-            if($objExcelSheetOpen.cells.Item(2, 2).text -eq ""){
-                $objExcelSheetOpen.cells.Item(2, 2) = "X"  
-            }
-            if($objExcelSheetOpen.cells.Item(3, 2).text -eq ""){
-                $objExcelSheetOpen.cells.Item(3, 2) = "X"  
-            }
-            if($objExcelSheetOpen.cells.Item(4, 2).text -eq ""){
-                $objExcelSheetOpen.cells.Item(4, 2) = "X"
-            }
-        }
+# Affichage tableau détail
+write-host "Affichage tableau détail"
+$nbredetail = $nbreref * $nbreanalyse
+write-host "Nombre détail"
+write-host $nbredetail
 
-        if($objExcelSheetOpen.cells.Item($i, 3).text -eq ""){
-            if($objExcelSheetOpen.cells.Item(2, 3).text -eq ""){
-                $objExcelSheetOpen.cells.Item(2, 3) = "X"  
-            }
-            if($objExcelSheetOpen.cells.Item(3, 3).text -eq ""){
-                $objExcelSheetOpen.cells.Item(3, 3) = "X"  
-            }
-            if($objExcelSheetOpen.cells.Item(4, 3).text -eq ""){
-                $objExcelSheetOpen.cells.Item(4, 3) = "X"
-            }
-        }
+$i=0
 
-        if($objExcelSheetOpen.cells.Item($i, 4).text -eq ""){
-            if($objExcelSheetOpen.cells.Item(2, 4).text -eq ""){
-                $objExcelSheetOpen.cells.Item(2, 4) = "X"  
-            }
-            if($objExcelSheetOpen.cells.Item(3, 4).text -eq ""){
-                $objExcelSheetOpen.cells.Item(3, 4) = "X"  
-            }
-            if($objExcelSheetOpen.cells.Item(4, 4).text -eq ""){
-                $objExcelSheetOpen.cells.Item(4, 4) = "X"
+for($i=0; $i -lt $nbreref; $i++){
+    for ($j=0; $j -le $nbreanalyse; $j++){
+     write-host $tabref[$i,$j]
+    }
+}
+
+# Relecture fichier Excel feuil2 pour trouver l'indice de la ligne échantillon dans le tableau
+for($k=1; $k -le $rows2; $k++){
+    $lecturereference = $objExcelSheetOpen2.cells.Item($k, 1).text # Lis la premiére colonne
+    $lectureanalyse = $objExcelSheetOpen2.cells.Item($k, 2).text # Lis la deuxiéme colonne est ainsi de suite
+    $lectureparametre = $objExcelSheetOpen2.cells.Item($k, 3).text
+    $lecturevaleur = $objExcelSheetOpen2.cells.Item($k, 4).text
+    # Lecture du tableau détail
+    # On cherche la ligne de la référence (échantillon)
+    for($i=0; $i -lt $nbreref; $i++){
+        if ($tabref[$i,0] -eq $lecturereference){
+            # On cherche la position analyse si on l'a trouvé 
+            for($j=0; $j -le $nbreanalyse; $j++){
+                if ($analyse[$j] -eq $lectureanalyse){
+                    $u=$j+1
+                    if ($analyse[$u] -eq $lectureparametre){
+                        $tabref[$i,$u] = $analyse[$j]
+                        $v=$u+1
+                        $tabref[$i,$v] = $analyse[$u]
+                        $z=$v+1
+                        $tabref[$i,$z] = $lecturevaleur
+                    }
+                 }
             }
         }
     }
+}
 
-    # $objExcelSheetOpen.cells.Item(1, 5) = 'X' # Mets une croix dans la ligne 1 colonne 5
+# Affichage tableau détail final
+write-host "Affichage tableau détail final"
+write-host "------------------------------"
+write-host "Nombre détail"
+write-host $nbredetail
 
-    $objExcelFileOpen.close() # On ferme le fichier Excel
-    $objExcel.Quit()
+$i=0
+# On affiche les valeurs du tableau tabref
+for($i=0; $i -lt $nbreref; $i++){
+    for ($j=0; $j -le $nbreanalyse; $j++){
+     write-host $tabref[$i,$j]
+    }
+}
+
+write-host "Fin affichage tableau détail final"
+
+# Initialisation du fichier excel
+
+$objExcel = new-object -comobject excel.application 
+$objExcel.Visible = $True 
+$FinalExcelLocation = "C:\Users\Obsti\Documents\Resultat.xlsx"
+
+# Création du fichier excel
+
+if (Test-Path $FinalExcelLocation)  
+{  
+    # On ouvre le fichier excel
+    $finalWorkBook = $objExcel.WorkBooks.Open($FinalExcelLocation)  
+    $finalWorkSheet = $finalWorkBook.Worksheets.Item(1)  
+
+}
+else {  
+    # On crée le fichier excel
+    $finalWorkBook = $objExcel.Workbooks.Add()  
+    $finalWorkSheet = $finalWorkBook.Worksheets.Item(1)
+}
+
+$i=0
+$t=$i+1
+# On mets les valeurs dans le tableau excel
+for($i=0; $i -lt $nbreref; $i++){
+    $t=$i+1
+    $a=1
+    for ($j=0; $j -le $nbreanalyse; $j++){
+        $finalWorkSheet.Cells.Item($t,$a) =$tabref[$i,$j]
+        $a++ 
+    }
+}
+
+$finalWorkBook.SaveAs($FinalExcelLocation) # On sauvegarde le fichier excel
+
+$objExcelFileOpen.close()
+$objExcel.Quit()
